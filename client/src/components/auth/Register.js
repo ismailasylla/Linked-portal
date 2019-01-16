8130cb6ee9e7ab046bc8e42751876e4c4de9c075
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-16 20:19:56 +0530

    connecting the front-end client-side to the back-end api

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    console.log(newUser);
+  }
+
+  render() {
+    return (
+      <div class="register">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 m-auto">
+              <h1 class="display-4 text-center">Sign Up</h1>
+              <p class="lead text-center">Create your DevConnector account</p>
+              <form onSubmit={this.onSubmit}>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="email"
+                    class="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    class="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    class="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" class="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Register;
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer class="bg-info text-white mt-5 p-4 text-center">
+      Copyright &copy; 2019 Student Portail
+    </footer>
+  );
+}
+
+export default Footer;
