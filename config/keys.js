1d8c41cafb9dc06b43f7c5dfe89c3d8d2dd831c3
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-20 15:42:31 +0530

    Securing the Keys for for application deployment on heroku and git

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://isma:portail12345@ds157834.mlab.com:57834/portail',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
